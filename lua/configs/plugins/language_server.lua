2200e320f0f70b853c4ffc043ceb4df038c7aafe
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-06-01 18:22:20 +0200

    Added completition

--- lua/configs/plugins/language_server.lua
+++ lua/configs/plugins/language_server.lua
@@ -6,10 +6,17 @@ return {
 	lspconf_setup = function()
 		local lspconfig = require("lspconfig")
 		local lazypath = vim.fn.stdpath("data") .. "\\lazy\\"
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
 			cmd = { lazypath .. "lua-language-server\\bin\\lua-language-server.exe" },
 		})
-		lspconfig.pyright.setup({})
+
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+		})
+
 		require("configs.keymaps.language_server")
 	end,
 }
