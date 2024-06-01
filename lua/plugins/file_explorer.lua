a1de88a3b4bf0bce6393ebc7062c622814b2be0d
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-06-01 20:44:54 +0200

    Added Dashboard

--- lua/plugins/file_explorer.lua
+++ lua/plugins/file_explorer.lua
@@ -14,6 +14,14 @@ return {
 		config = function()
 			require("configs.keymaps.file_explorer")
 			require("telescope").setup({
+				pickers = {
+					find_files = {
+						layout_config = {
+							prompt_position = "top",
+						},
+						sorting_strategy = "ascending",
+					},
+				},
 				defaults = {
 					file_ignore_patterns = {
 						"node_modules",
