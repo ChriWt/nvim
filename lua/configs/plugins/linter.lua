9d8ffe37942c34005c3fa972dc08a41d9b1c9ac9
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-06-01 15:14:01 +0200

    Added python formatting

--- lua/configs/plugins/linter.lua
+++ lua/configs/plugins/linter.lua
@@ -2,6 +2,9 @@ local null_ls = require("null-ls")
 
 return {
 	sources = {
-		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.stylua, -- formatting for lua
+
+    null_ls.builtins.formatting.isort,  -- import sorting python
+    null_ls.builtins.formatting.black,  -- formatting for python
 	},
 }
