3de7dc9be3a9cc768c33f1ce11e37d2516f1075f
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-06-03 01:06:32 +0200

    Changed event type

--- lua/plugins/dimmer.lua
+++ lua/plugins/dimmer.lua
@@ -1,7 +1,7 @@
 return {
 	"levouh/tint.nvim",
 	lazy = true,
-	event = "VimEnter",
+	event = "VeryLazy",
 	config = function()
 		require("tint").setup(
       require("configs.plugins.dimmer")
--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,7 +1,7 @@
 return {
 	"feline-nvim/feline.nvim",
 	lazy = true,
-	event = "VimEnter",
+	event = "VeryLazy",
 	config = function()
 		require("feline").setup(require("configs.plugins.statusline"))
 	end,
