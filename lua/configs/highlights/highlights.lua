da2a7d28dc81a96d2ef3a83f9a20dd298208d906
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-06-03 02:41:24 +0200

    Removed comments

--- lua/configs/highlights/highlights.lua
+++ lua/configs/highlights/highlights.lua
@@ -2,9 +2,7 @@ vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#98c379" })
 vim.api.nvim_set_hl(0, "DashboardFooter", { fg = "#98c379" })
 
 vim.api.nvim_set_hl(0, "DashboardProjectTitle", { fg = "#e06c75" })
--- vim.api.nvim_set_hl(0, "DashboardProjectTitleIcon", { fg = "#f6d5a4" })
 
 vim.api.nvim_set_hl(0, "DashboardMruTitle", { fg = "#e06c75" })
--- vim.api.nvim_set_hl(0, "DashboardMruIcon", { fg = "#f6d5a4" })
 
 vim.api.nvim_set_hl(0, "DashboardFiles", { fg = "#f6d5a4" })
