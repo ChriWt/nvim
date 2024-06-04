b62031c83abea6415941f0110b1687c2f3101afa
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-06-04 17:46:55 +0200

    Prevented LuaSnip from starting in non lua projects

--- lua/configs/plugins/completition.lua
+++ lua/configs/plugins/completition.lua
@@ -1,48 +1,52 @@
+local function goNext(fallback)
+  local cmp = require("cmp")
+  if cmp.visible() then
+    -- If only one option is present choose that one
+    if #cmp.get_entries() == 1 then
+      cmp.confirm({ select = true })
+    else
+      cmp.select_next_item()
+    end
+  else
+    fallback()
+  end
+end
+
+local function goPrevious(fallback)
+  local cmp = require("cmp")
+  if cmp.visible() then
+    cmp.select_prev_item()
+  else
+    fallback()
+  end
+end
+
 return function()
-	local cmp = require("cmp")
-	require("luasnip.loaders.from_vscode").lazy_load()
+  local cmp = require("cmp")
 
-	cmp.setup({
-		snippet = {
-			expand = function(args)
-				require("luasnip").lsp_expand(args.body)
-			end,
-		},
-		window = {
-			completion = cmp.config.window.bordered(),
-			documentation = cmp.config.window.bordered(),
-		},
-		mapping = cmp.mapping.preset.insert({
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-          -- If only one option is present choose that one
-					if #cmp.get_entries() == 1 then
-						cmp.confirm({ select = true })
-					else
-						cmp.select_next_item()
-					end
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-e>"] = cmp.mapping.abort(),
-			["<CR>"] = cmp.mapping.confirm({ select = true }),
-		}),
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp" },
-			{ name = "luasnip" },
-		}, {
-			{ name = "buffer" },
-		}),
-	})
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<Tab>"] = cmp.mapping(goNext, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(goPrevious, { "i", "s" }),
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+    }, {
+      { name = "buffer" },
+    }),
+  })
 end
