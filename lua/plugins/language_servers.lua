369e17e2ee4a7cc5f87f3c1d0c3ba0000aa437b3
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-06-04 16:55:21 +0200

    Fixed language server startup

--- lua/plugins/language_servers.lua
+++ lua/plugins/language_servers.lua
@@ -1,28 +1,28 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = true,
+  {
+    "williamboman/mason.nvim",
+    lazy = true,
     cmd = { "Mason", "MasonInstallAll", "MasonInstall", "MasonUpdate" },
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = true,
-		event = "InsertEnter",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = require("configs.plugins.language_server").ensure_installed,
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = true,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = true,
+    event = "BufReadPre",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = require("configs.plugins.language_server").ensure_installed,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
     cmd = { "LspInfo" },
     keys = { "gd" },
-		event = "InsertEnter",
-		config = require("configs.plugins.language_server").lspconf_setup,
-	},
+    event = "InsertEnter",
+    config = require("configs.plugins.language_server").lspconf_setup,
+  },
 }
