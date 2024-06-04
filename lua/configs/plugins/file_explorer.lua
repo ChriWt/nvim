0c6d9c290e909beb0dda4028ca580ea083752860
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-06-04 18:50:28 +0200

    Fixed telescope orientation

--- lua/configs/plugins/file_explorer.lua
+++ lua/configs/plugins/file_explorer.lua
@@ -2,14 +2,6 @@ return {
 	telescope_config = function()
 		require("configs.keymaps.file_explorer")
 		require("telescope").setup({
-			pickers = {
-				find_files = {
-					layout_config = {
-						prompt_position = "top",
-					},
-					sorting_strategy = "ascending",
-				},
-			},
 			defaults = {
 				file_ignore_patterns = {
 					"node_modules",
--- lua/configs/plugins/pyvenv.lua
+++ lua/configs/plugins/pyvenv.lua
@@ -2,6 +2,20 @@ return {
 	telescope_conf = function()
 		require("configs.keymaps.file_explorer")
 		require("telescope").setup({
+			pickers = {
+				find_files = {
+					layout_config = {
+						prompt_position = "top",
+					},
+					sorting_strategy = "ascending",
+				},
+        live_grep = {
+					layout_config = {
+						prompt_position = "top",
+					},
+					sorting_strategy = "ascending",
+        }
+			},
 			defaults = {
 				file_ignore_patterns = {},
 			},
