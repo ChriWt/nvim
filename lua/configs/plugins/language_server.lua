39dc88c797feceeac5e31764e9cc5a0b85f0b6e7
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-06-10 00:32:03 +0200

    Added on_attach to lsp

--- lua/configs/plugins/language_server.lua
+++ lua/configs/plugins/language_server.lua
@@ -10,15 +10,18 @@ return {
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		lspconfig.lua_ls.setup({
+      on_attach = on_attach,
 			capabilities = capabilities,
 			cmd = { lazypath .. "lua-language-server\\bin\\lua-language-server.exe" },
 		})
 
 		lspconfig.pyright.setup({
+      on_attach = on_attach,
 			capabilities = capabilities,
 		})
 
 		lspconfig.tsserver.setup({
+      on_attach = on_attach,
 			capabilities = capabilities,
 		})
 
