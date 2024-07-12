33e19b42a002f38331258fef0122ed8026c05507
Christian Bu Toledo <christianbutoledo@yahoo.it>
2024-07-12 11:05:14 +0200

    Keybindings updates

--- lua/configs/keymaps/language_server.lua
+++ lua/configs/keymaps/language_server.lua
@@ -1,15 +1,10 @@
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go To Definition", noremap = true, silent = true })
+
 vim.keymap.set(
 	{ "n", "v" },
 	"<leader>ck",
 	vim.lsp.buf.hover,
 	{ desc = "Get info", noremap = true, silent = true }
-)
-
-vim.keymap.set(
-	{ "n", "v" },
-	"<leader>cd",
-	vim.lsp.buf.definition,
-	{ desc = "Go To Definition", noremap = true, silent = true }
 )
 
 vim.keymap.set(
