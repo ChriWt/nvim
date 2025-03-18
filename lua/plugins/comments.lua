26164a49d67e45cde5aad2470fe55ef2d4bef08c
ChriWt <christianbutoledo@yahoo.it>
2025-03-18 17:41:36 +0100

    Changed event that enables comments

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,10 +1,11 @@
 return {
-	"numToStr/Comment.nvim",
-	lazy = true,
-  keys = { "<leader>/" },
-	config = function()
-		require("Comment").setup(
-      require("configs.plugins.comments")
-    )
-	end,
+    "numToStr/Comment.nvim",
+    lazy = true,
+	event = "BufReadPre",
+    keys = { "<leader>/" },
+    config = function()
+        require("Comment").setup(
+            require("configs.plugins.comments")
+        )
+    end,
 }
